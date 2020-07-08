97cc90c4ee6713ea2547502194136214d58f2227
Tiago Santos <tiagoltj@hotmail.com>
2020-07-07 21:16:42 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+'use strict'
+
+window.onload = function(){
+    const $btn_saiba = document.getElementById("btn_saiba");
+
+    $btn_saiba.addEventListener('click',  function onClick(e){
+        alert("Somente teste")
+     
+     });
+
+}
+
+
+
+
+
+
+
